1ac8dac28e4290e1e903f562b6810632172e2340
Éric Fritzen Valle <ericfritzenvalle@gmail.com>
2025-11-13 17:10:36 -0300

    feat: adiciona componente 'page' para o layout principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Page} from './components/shared/page/page';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
+  imports: [
+    Page
+  ],
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-masterclass');
+
 }
